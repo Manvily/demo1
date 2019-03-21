65073f32ce716379780e504f504c1c30a8220237
Mariusz Kwiatek PC <ototto@protonmail.com>
2019-03-21 22:21:19 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/demo1/Demo1Application.kt
@@ -0,0 +1,12 @@
+package com.example.demo1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo1Application
+
+fun main(args: Array<String>) {
+	runApplication<Demo1Application>(*args)
+	println("Hello World")
+}
